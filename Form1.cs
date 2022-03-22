0fdb28e79031dfea2107bfe86a28c55fcd857b94
majorjk228 <sana-ivanov1@yandex.ru>
2022-03-22 13:01:05 +0300

    123

--- Form1.cs
+++ Form1.cs
@@ -36,7 +36,6 @@ namespace SenderEmail
 
                 string[] test = File.ReadAllLines("text.txt"); //беру из файла все строки с емаийлами
                 label1.Text = File.ReadAllText("text.txt");
-                MessageBox.Show("da");
 
                 // add from,to mailaddresses
 
